3934d870823d5d540731ad0ed0f82e52c4d996fb
ali_askar <ali.askar@limitlesstech.ae>
2026-04-23 18:20:30 +0400

    admin title

--- core/urls.py
+++ core/urls.py
@@ -12,6 +12,7 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-admin.site.site_title = "I-Chancy Admin"
-admin.site.site_header = "I-Chancy Telegram Bot"
+admin.site.site_title = "ai-career-guidance-coach Admin"
+admin.site.site_header = "AI-Career Guidance Coach"
 admin.site.index_title = "Admin Dashboard"
+
